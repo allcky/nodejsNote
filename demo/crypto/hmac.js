c50955c257a564c1c9b3a1de8b4d6e2b2b07e1a4
allcky <allcky@qq.com>
2017-07-03 16:45:51 +0800

    1

--- demo/crypto/hmac.js
+++
@@ -1,4 +0,0 @@
-var crypto = require('crypto');
-var hmac = crypto.createHmac('sha1','334342');
-hmac.update('12345');
-console.log(hmac.digest('hex'))
--- demo/stream/1.readStream.js
+++ demo/stream/1.readStream.js
@@ -1,17 +1,13 @@
 var fs = require('fs');
-var rs = fs.createReadStream('./a33.html',{
+var rs = fs.createReadStream('./a.html',{
     'encoding':'utf8',
     "highWaterMark":10
 });
 //设置读取后数据编码方式 默认buffer
 // rs.setEncoding('utf8')
 rs.on('data',function (data) {
-    console.log(rs.bytesRead)
     console.log(data)
 });
 rs.on('end',function () {
     console.log('end')
-})
-rs.on('error',function (error) {
-    console.error(error)
 })
